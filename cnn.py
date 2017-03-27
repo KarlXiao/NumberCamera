71db312afd28bcbf89cd9fe78f9259ad1a0d4d1c
Karl <270527455@qq.com>
2017-03-27 11:21:08 +0800

    CNN

--- cnn.py
+++ cnn.py
@@ -2,6 +2,7 @@
 train cnn model for number recognition
 """
 import numpy as np
+import math
 from six.moves import cPickle as pickle
 import time
 import tensorflow as tf
@@ -61,40 +62,93 @@ with graph.as_default():
         
     # model
     def model(data, weight, bias, dropout=1):
-        conv1 = conv2d(data, weight['w1'], bias['b1'], 5, 1, 'VALID', 'conv1')
-        pool1 = tf.nn.max_pool(conv1, 2, 2, 'VALID')
+        conv1 = conv2d(data, weight['w1'], bias['b1'], 1, 'VALID', 'conv1')
+        pool1 = tf.nn.max_pool(conv1, [1, 2, 2, 1], [1, 2, 2, 1], 'VALID')
+        norm1 = batch_norm(pool1, 'norm1')
 
-        conv2 = conv2d(pool1, weight['w2'], bias['b2'], 1, 'VALID', 'conv2')
-        pool2 = tf.nn.max_pool(conv2, 2, 2, 'VALID')
+        conv2 = conv2d(norm1, weight['w2'], bias['b2'], 1, 'VALID', 'conv2')
+        pool2 = tf.nn.max_pool(conv2, [1, 2, 2, 1], [1, 2, 2, 1], 'VALID')
+        norm2 = batch_norm(pool2, 'norm2')
 
-        shape = pool2.get_shape().as_list()
-        reshape = tf.reshape(pool2, [shape[0], shape[1]*shape[2]*shape[3]])
-        fc1 = tf.nn.relu(tf.matmul(pool2, weight['w3'])+bias['b3'])
-        out = tf.matmul(fc1, weight['w4'])+bias['b4']
+        shape = norm2.get_shape().as_list()
+        reshape = tf.reshape(norm2, [-1, shape[1]*shape[2]*shape[3]])
+        fc1 = tf.nn.relu(tf.matmul(reshape, weight['w3'])+bias['b3'])
+        fc2 = tf.nn.relu(tf.matmul(fc1, weight['w4'])+bias['b4'])
+        out = tf.matmul(fc2, weight['w5'])+bias['b5']
         return out
 
     # parameter
     weight = {
-        'w1': tf.Variable(tf.random_normal([5, 5, num_channels, 6])),
-        'w2': tf.Variable(tf.random_normal([5, 5, 6, 16])),
-        'w3': tf.Variable(tf.random_normal([16*5*5, 120])),
-        'w4': tf.Variable(tf.random_normal([120, 84]))
+        'w1': tf.Variable(tf.random_normal([5, 5, num_channels, 20])),
+        'w2': tf.Variable(tf.random_normal([5, 5, 20, 50])),
+        'w3': tf.Variable(tf.random_normal([50*5*5, 500])),
+        'w4': tf.Variable(tf.random_normal([500, 84])),
+        'w5': tf.Variable(tf.random_normal([84, num_labels]))
     }
 
     bias = {
-        'b1': tf.Variable(tf.random_normal([6])),
-        'b2': tf.Variable(tf.random_normal([16])),
-        'b3': tf.Varibale(tf.random_normal([120])),
-        'b4': tf.Variable(tf.random_normal([84]))
+        'b1': tf.Variable(tf.random_normal([20])),
+        'b2': tf.Variable(tf.random_normal([50])),
+        'b3': tf.Variable(tf.random_normal([500])),
+        'b4': tf.Variable(tf.random_normal([84])),
+        'b5': tf.Variable(tf.random_normal([num_labels]))
     }
 
-    logits = model(tf_data, weight, bias)
+    logits = model(tf_data, weight, bias, keep_prob)
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_labels))
+    tf.summary.scalar('Training loss', loss)
 
     # optimizer
-    global_setp = tf.Variable(0)
-    learning_rate = tf.train.exponential_decay(0.1, global_setp, 100, 0.95, staircase=True)
-    tf.summary.scale('Learning rate', learning_rate)
-    optimizer = tf. train.GradientDescentOptimizer(learning_rate).minimize(loss, global_setp=global_setp)
+    global_step = tf.Variable(0)
+    learning_rate = tf.train.exponential_decay(0.01, global_step, 100, 0.9, staircase=True)
+    tf.summary.scalar('Learning rate', learning_rate)
+    optimizer = tf. train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
-    correct_pre = tf.
+    # accuracy
+    correct_pre = tf.equal(tf.argmax(logits, axis=1), tf.argmax(tf_labels, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pre, tf.float32))
+    tf.summary.scalar('Training accuracy', accuracy)
+
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('./logs')
+    saver = tf.train.Saver()
+
+num_steps = 20000
+batch_size = 64
+
+with tf.Session(graph=graph) as session:
+    session.run(tf.global_variables_initializer())
+    writer.add_graph(graph=session.graph)
+    print('Initialized')
+    start_time = time.time()
+    for step in range(num_steps):
+        train_offset = step * batch_size % (train_data.shape[0] - batch_size)
+        batch_data = train_data[train_offset:(train_offset+batch_size), :, :, :] - train_mean
+        batch_label = train_labels[train_offset:(train_offset+batch_size), :]
+        feed_dict={tf_data: batch_data, tf_labels: batch_label, keep_prob: 0.5}
+        _, l, train_accuracy, summary = session.run([optimizer, loss, accuracy, merged], feed_dict=feed_dict)
+        writer.add_summary(summary, step)
+        if step % 1000 == 0:
+            valid_accuracy = 0
+            for i in range(math.ceil(valid_data.shape[0] / batch_size)):
+                valid_offset = i * batch_size % (valid_data.shape[0] - batch_size)
+                feed_dict = {tf_data: valid_data[valid_offset:(valid_offset+batch_size), :, :, :] - valid_mean, 
+                            tf_labels: valid_labels[valid_offset:(valid_offset+batch_size)], keep_prob: 1.}
+                step_accuracy = session.run(accuracy, feed_dict=feed_dict)
+                valid_accuracy = valid_accuracy + step_accuracy
+            print('Batch loss at step %d: %f' % (step, l))
+            print('Training Batch accuray: %f%%' % (train_accuracy * 100))
+            print('Valid accuracy: %f%%' % (100 * valid_accuracy / math.floor(valid_data.shape[0] / batch_size)))
+    print('Training costs %f seconds.' % (time.time()-start_time))
+    saver.save(session, './models/cnn_numbercamera.tfmodel')
+
+with tf.Session(graph=graph) as session:
+    saver.restore(session, './models/cnn_numbercamera.tfmodel')
+    test_accuracy = 0
+    for i in range(math.ceil(test_data.shape[0] / batch_size)):
+        test_offset = i * batch_size % (test_data.shape[0] - batch_size)
+        feed_dict = {tf_data: test_data[test_offset:(test_offset+batch_size), :, :, :] - train_mean, 
+                    tf_labels: test_labels[test_offset:(test_offset+batch_size)], keep_prob: 1.}
+        step_accuracy = session.run(accuracy, feed_dict=feed_dict)
+        test_accuracy = test_accuracy + step_accuracy
+    print('Test accuray: %f%%' % (100 * test_accuracy / math.floor(test_data.shape[0] / batch_size)))
